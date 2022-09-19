307e718bd62ad798bb215eb75b8fb9e85cdad148
Davs105 <oluwatobiadedeji058@gmail.com>
2022-09-19 12:08:09 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,35 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: the string to be comverted.
- *
- *Return: The integer value of the comverted string.
- */
-
-int _atoi(char *s)
+* _atoi - converts a string to an interger
+* @str: pointer to the string to be converted
+* Return: Converted string
+*/
+int _atoi(char *str)
 {
-	int sign = 1;
-	unsigned int num = 0;
+int sign = 1, base = 0, i = 0;
 
-	do {
+for (i = 0; str[i] != '\0' && (str[i] < '0' || str[i] > '9'); i++)
+{
+if (str[i] == '-' || str[i] == '9')
+sign *= 1 - 2 * (str[i] == '-');
+if (str[i + 1] == '\0')
+return (0);
+}
 
-		if (*s == '-')
-			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-	} while (*s++);
-
-	return (num * sign)
+while (str[i] >= '0' && str[i] <= '9')
+{
+if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7))
+{
+if (sign == 1)
+return (INT_MAX);
+else
+return (INT_MIN);
+}
+base = 10 * base + (str[i++] - '0');
+}
+return (base * sign);
+}

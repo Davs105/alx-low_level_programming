2ef623c44e7674f868e07eed8db7001c05d5292f
Davs105 <oluwatobiadedeji058@gmail.com>
2022-10-12 10:28:36 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,15 +1,19 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
- */
+* struct op - Struct op
+* @op: The operator
+* @f: The function associated
+*/
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,67 +1,56 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The sum of a and b.
- */
+* op_add - Add two numbers
+* @a: The first number
+* @b: The second number
+* Return: The sum of a and b
+*/
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 
 /**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The difference of a and b.
- */
+* op_sub - Finds the difference of two numbers
+* @a: The first number, a parameter
+* @b: The second number, a param
+* Return: The diff of a and b
+*/
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 
 /**
- * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The product of a and b.
- */
+* op_mul - Multiplies two numbers
+* @a: The first number, a param
+* @b: The second number, a param
+* Return: The product of a and b
+*/
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 
 /**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The quotient of a and b.
- */
+* op_div - Divides two numbers
+* @a: The first number, a param
+* @b: The second number, a param
+* Return: The quotient of a and b
+*/
 int op_div(int a, int b)
 {
-	return (a / b);
+return (a / b);
 }
 
 /**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The remainder of the division of a by b.
- */
+* op_mod - Finds the modulus of two numbers
+* @a: The first number, a param
+* @b: The second number, a param
+* Return: The modulus of a and b
+*/
 int op_mod(int a, int b)
 {
-	return (a % b);
+return (a % b);
 }
